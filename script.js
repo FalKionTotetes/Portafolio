12259bf3b8ee1d82fe6770c9b94fe567c9adb8f8
FalKionTotetes <etapia2505@gmail.com>
2025-07-02 10:45:23 -0400

    a

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ document.getElementById("infopersonal").scrollIntoView({ behavior: "smooth" });
 function irBoton2(){
 document.getElementById("habilidades").scrollIntoView({ behavior: "smooth" });
 }
+function irBoton3(){
+    document.getElementById("contacto").scrollIntoView({behavior:"smooth"});
+}
